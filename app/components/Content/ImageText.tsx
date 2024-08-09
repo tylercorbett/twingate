9701de128c863e484b815103e922cf2f68d447cf
Tyler Corbett <thetylercorbett@gmail.com>
2024-08-09 15:48:15 -0700

    fix square reverse

--- app/components/Content/ImageText.tsx
+++ app/components/Content/ImageText.tsx
@@ -24,14 +24,14 @@ export default function ImageText({
           leftToRight ? "flex-row" : "flex-row-reverse"
         } items-center max-h-40 `}
       >
-        <div className="w-[50%]">
+        <div className="w-[25%] aspect-square">
           <img
             src={imageURI}
             alt={title || "Image"}
             className="object-cover w-full h-full"
           />
         </div>
-        <div className="ml-4 w-[50%] flex flex-col justify-start h-full overflow-hidden">
+        <div className="ml-4 w-[75%] flex flex-col justify-start h-full overflow-hidden">
           {title && <h3 className="font-bold text-lg mb-2">{title}</h3>}
           <p className="text-gray-700 overflow-hidden leading-tight text-ellipsis-line max-h-28">
             {text}
