b92e4b9a66b99ef6820b2e9bb4412a439e506d4a
Tyler Corbett <thetylercorbett@gmail.com>
2024-08-09 15:46:15 -0700

    better image text

--- app/components/Content/ImageText.tsx
+++ app/components/Content/ImageText.tsx
@@ -20,20 +20,20 @@ export default function ImageText({
       className="p-4 border border-black overflow-hidden bg-white"
     >
       <div
-        className={`flex overflow-hidden p-3 h-full ${
+        className={`flex overflow-hidden p-3 gap-2 h-full ${
           leftToRight ? "flex-row" : "flex-row-reverse"
         } items-center max-h-40 `}
       >
-        <div className="w-40">
+        <div className="w-[50%]">
           <img
             src={imageURI}
             alt={title || "Image"}
             className="object-cover w-full h-full"
           />
         </div>
-        <div className="ml-4 flex-grow flex flex-col justify-start h-full overflow-hidden">
+        <div className="ml-4 w-[50%] flex flex-col justify-start h-full overflow-hidden">
           {title && <h3 className="font-bold text-lg mb-2">{title}</h3>}
-          <p className="text-gray-700 overflow-hidden leading-tight text-ellipsis-line">
+          <p className="text-gray-700 overflow-hidden leading-tight text-ellipsis-line max-h-28">
             {text}
           </p>
         </div>
