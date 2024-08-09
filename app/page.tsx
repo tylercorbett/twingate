1f9f2ab8fafe29ec2dd7e4a1df3608bf61153c17
Tyler Corbett <thetylercorbett@gmail.com>
2024-08-09 15:37:12 -0700

    change height

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     <main className="h-screen overflow-hidden">
       <div className="flex h-full p-4 gap-4">
         {/* Left Side: JSON Textarea */}
-        <div className="w-1/2 h-screen">
+        <div className="w-1/2 h-[98%]">
           <textarea
             className={`w-full h-full p-4 border ${
               hasError ? "border-red-500" : "border-gray-300"
@@ -29,7 +29,7 @@ export default function Home() {
         </div>
 
         {/* Right Side: Content Area with Border */}
-        <div className=" w-1/2 h-screen overflow-y-auto px-4 py-2 border-4 border-gray-400">
+        <div className=" w-1/2 h-[98%] overflow-y-auto px-4 py-2 border-4 border-gray-400">
           {hasError ? (
             <p className="text-red-500 font-bold">
               Invalid JSON format. Please check your input.
