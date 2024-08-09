55b0ed4013eb37a77d59d192cc22c9641ef66265
Tyler Corbett <thetylercorbett@gmail.com>
2024-08-09 14:28:44 -0700

    formatting

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,10 @@ export default function Home() {
   }, [jsonText]);
 
   return (
-    <main className="min-h-screen">
-      <div className="flex h-full">
+    <main className="h-screen overflow-hidden">
+      <div className="flex h-full p-4 gap-4">
         {/* Left Side: JSON Textarea */}
-        <div className="w-1/2 p-4 h-screen">
+        <div className="w-1/2 h-screen">
           <textarea
             className={`w-full h-full p-4 border ${
               hasError ? "border-red-500" : "border-gray-300"
@@ -33,19 +33,21 @@ export default function Home() {
         </div>
 
         {/* Right Side: Content Area with Border */}
-        <div className="w-1/2 p-4 flex items-center justify-center">
-          <div className="w-full h-full py-3 px-4 border-4 border-gray-400 flex-col justify-center gap-4">
-            {hasError ? (
-              <p className="text-red-500 font-bold">
-                Invalid JSON format. Please check your input.
-              </p>
-            ) : (
-              <>
-                <HeroImage imageURI="https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg" />
-                <HeroImage imageURI="https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J" />
-              </>
-            )}
-          </div>
+        <div className=" w-1/2 h-screen overflow-y-auto px-4 border-4 border-gray-400 flex flex-col justify-start gap-4">
+          {hasError ? (
+            <p className="text-red-500 font-bold">
+              Invalid JSON format. Please check your input.
+            </p>
+          ) : (
+            <>
+              <HeroImage imageURI="https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg" />
+              <HeroImage imageURI="https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J" />
+              {/* <HeroImage imageURI="https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J" />
+              <HeroImage imageURI="https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J" />
+              <HeroImage imageURI="https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J" />
+              <HeroImage imageURI="https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J" /> */}
+            </>
+          )}
         </div>
       </div>
     </main>
